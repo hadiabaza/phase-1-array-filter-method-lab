17739e3cba56eae459465212055f6e7c6d842bb8
Hadi Abaza <Hadiabaza1@gmail.com>
2021-06-09 17:37:13 -0400

    commit tree 7846d1d8234b5ae4b14f273b6e8411b3e30d202a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, person) {
+  return array.filter((name) => name.toLowerCase() === person.toLowerCase());
+  //return array.find((name) => name.toLowerCase() === person.toLowerCase());
+}
+//return array.find((name) => name === person);
+// HOW DO WE MAKE A COMMON DENOMINATOR?
+//TEST CASE SAYS CASE DOESN'T HAVE TO MATCH BUT LETTERS DO
+//TO CHECK IF PERSON === NAME:
+// TOLOWERCASE() OR TOUPPERCASE()
+
+function fuzzyMatch(array, src) {}
+
+function matchName(source, name) {
+  return array.filter((record) => record.name === name);
+}
